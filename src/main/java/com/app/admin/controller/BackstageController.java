e919a71d995e8e35b933e3d6146e267723cff254
liulinfa123 <37287317+liulinfa123@users.noreply.github.com>
2018-11-06 11:08:18 +0800

    commit log

--- src/main/java/com/app/admin/controller/BackstageController.java
+++ src/main/java/com/app/admin/controller/BackstageController.java
@@ -6,9 +6,11 @@ import com.app.exception.ResponseMessage;
 import com.app.utils.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 
+@Slf4j
 @Api(value = "测试接口1")
 @RestController
 @RequestMapping("/backstage")
@@ -17,6 +19,7 @@ public class BackstageController {
     @PostMapping("text")
     @ApiOperation(value = "测试接口",notes = "接口描述")
     public ResponseMessage text(@RequestBody GetTestDTO getTestDTO){
+        log.info(getTestDTO.getName());
         System.out.println(getTestDTO);
         return ResultUtil.success("you name is " + getTestDTO.getName());
     }
