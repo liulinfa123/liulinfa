6a8706e2908729704769b7a6b1ff03eb2ee20d20
liulinfa123 <37287317+liulinfa123@users.noreply.github.com>
2018-12-03 16:16:13 +0800

    commit login

--- src/main/java/com/app/config/WebMvcConfig.java
+++ src/main/java/com/app/config/WebMvcConfig.java
@@ -17,6 +17,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(tokenInterceptor).addPathPatterns("/admin/*");
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/admin/*").excludePathPatterns("/admin/backstageLogin");
     }
 }
